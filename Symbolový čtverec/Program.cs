ce7db9faae95edf84a57ca752066bce6fc9b16bf
drozboril <rozborildavid77@gmail.com>
2022-11-24 18:05:17 +0100

    Uživatel zadá znak a rozměry čtverce, program ho vypíše

---
+++ Symbolový čtverec/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Symbolový_čtverec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Napište znak");
+            string a = Console.ReadLine();
+            Console.WriteLine("Napište počet sloupců");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Napište počet znaků v řadě");
+            int c = int.Parse(Console.ReadLine());
+            for (int i = 0; i < b; i++) 
+            {
+                Console.SetCursorPosition(50, 10 + i);
+                for (int j = 0; j < c; j++)
+                {
+                    Console.Write("{0} ", a);
+                }
+                Console.Write("\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
